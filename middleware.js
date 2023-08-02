78e1049dea4b35afdd48cbc16c590b81775cbcad
ADITYA RAJ <raj8017933@gmail.com>
2023-08-03 00:06:19 +0530

    reviews author
    Author for reviews section made.

--- middleware.js
+++ middleware.js
@@ -1,6 +1,8 @@
-const { campgroundSchema,reviewSchema } = require('./schema.js');
+const { campgroundSchema, reviewSchema } = require('./schema.js');
 const ExpressError = require('./utils/ExpressError');
 const Campground = require('./models/campground');
+const Review = require('./models/review')
+
 module.exports.isLoggedIn = (req, res, next) => {
     //console.log("REQ.USER...",req.user);
     if (!req.isAuthenticated()) {
@@ -28,11 +30,21 @@ module.exports.validateCampground = (req, res, next) => {
     }
 }
 
-module.exports.isAuthor = async (req,res,next)=>{
+module.exports.isAuthor = async (req, res, next) => {
     const { id } = req.params;
     const campground = await Campground.findById(id);
-    if(!campground.author.equals(req.user._id)){
-        req.flash('error',"You don't have permission to do that!" );
+    if (!campground.author.equals(req.user._id)) {
+        req.flash('error', "You don't have permission to do that!");
+        return res.redirect(`/campgrounds/${id}`);
+    }
+    next();
+}
+
+module.exports.isReviewAuthor = async (req, res, next) => {
+    const { id, reviewId } = req.params;
+    const review = await Review.findById(reviewId);
+    if (!review.author.equals(req.user._id)) {
+        req.flash('error', "You don't have permission to do that!");
         return res.redirect(`/campgrounds/${id}`);
     }
     next();
